332c91ef3b5ee3f7fd07c2486581ee5bf0bf7a4c
Laura Villan <villan.laura@gmail.com>
2016-12-07 01:47:03 -0300

    cambios

--- controllers/producto.py
+++ controllers/producto.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 def index():
-    titulo = T(' Listado de productos' )
-    grid = SQLFORM.grid(db.producto, deletable = False, editable=False, details=True, csv = False)
+    titulo = T(' Listado de productos sin stock' )
+    #consulta = db.producto.cantidad == 0
+    #grid = crud.select(db.producto, consulta)
+    grid = SQLFORM.grid(db(db.producto.cantidad > 0), 
+                        deletable = False,
+                        editable=True,
+                        details=True,
+                        csv = True,
+                        exportclasses = dict(cvs = False, xml = False, csv_with_hidden_cols = False, tsv_with_hidden_cols = False,tsv = False )
+                       )
     return locals()
+
 
 
 
