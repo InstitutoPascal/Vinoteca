262936213adb70cfc95295db950f9d05a940ee96
LaLicalipso <villan.laura@gmail.com>
2016-12-09 01:33:57 -0300

    poblar Productos

--- controllers/producto.py
+++ controllers/producto.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 def index():
     titulo = T(' Listado de productos sin stock' )
-    #consulta = db.producto.cantidad == 0
-    #grid = crud.select(db.producto, consulta)
-    grid = SQLFORM.grid(db(db.producto.cantidad > 0),
+    grid = SQLFORM.grid(db(db.producto.cantidad == 0),
                         create = False,
                         deletable = True,
                         editable=True,
                         details=True,
                         csv = True,
-                        exportclasses = dict(cvs = False, xml = False, csv_with_hidden_cols = False, tsv_with_hidden_cols = False,tsv = False, json = False )
+                        exportclasses = dict(cvs = False,
+                                             xml = False,
+                                             csv_with_hidden_cols = False,
+                                             tsv_with_hidden_cols = False,
+                                             tsv = False,
+                                             json = False )
                        )
     return locals()
 
@@ -18,5 +21,10 @@ def index():
 
 def admin():
     titulo = T(' Administración de productos' )
-    grid = SQLFORM.grid(db.producto, deletable = False, csv = True)
+    grid = SQLFORM.grid(db.producto, deletable = False, csv = True,exportclasses = dict(cvs = False,
+                                                                                        xml = False,
+                                                                                        csv_with_hidden_cols = False,
+                                                                                        tsv_with_hidden_cols = False,
+                                                                                        tsv = False,
+                                                                                        json = False ))
     return locals()
